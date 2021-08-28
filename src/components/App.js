356f222e452e03ae7a7da06cefbc73f5a73eeaff
thucnhu <buitranthucnhu@gmail.com>
2021-08-28 20:08:18 +0700

    create app successfully

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import {  } from "./"
+
+
+export default function App() {
+    return (
+        <div>
+            <h1>Nhu</h1>
+        </div>
+    )
+}
